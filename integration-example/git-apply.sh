a6a1bd03830fbf4002f47c428aa739aca53af23d
Staffan Olsson <staffan@repos.se>
2018-12-14 17:41:55 +0100

    Runs in a new namespace every time

--- integration-example/git-apply.sh
+++ integration-example/git-apply.sh
@@ -17,8 +17,19 @@ git config user.name "Knative Training"
 date >> pushevenifunchanged.log
 git add --no-warn-embedded-repo .
 git commit -m "$(date)"
+rm pushevenifunchanged.log
 git push origin HEAD
 GITREV=$(git rev-parse HEAD)
 rm -rf ./.git
 
 #cat service-build.yaml | sed "s/revision: master/revision: $GITREV/" | kubectl apply -f -
+
+# Can we use k8s' generateName from kubectl
+namespace=pipelinerun-$(openssl rand -hex 4)
+
+kubectl create namespace $namespace
+kubectl -n $namespace apply -f ../caching-kaniko-build.yaml
+kubectl -n $namespace apply -f ../npm-export.yaml
+kubectl -n $namespace apply -f ./pipeline.yaml
+kubectl -n $namespace apply -f ./pipeline-resources.yaml
+kubectl -n $namespace apply -f ./pipeline-run.yaml
